30882dc16b7d2ffb07f502c73fcb05f8c0f4466c
X3l1m <edibselim1@gmail.com>
2023-04-24 17:35:55 +0200

    ev

--- domat/domates.c
+++ domat/domates.c
@@ -1,26 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct domates
+typedef struct s_stack
 {
-	int patates;
-	struct domates *next;
-}stack;
+    int value;
+    struct s_stack *next;
+} t_stack;
 
-stack	*create(void)
-{ 
-    stack	*i;
-	int	h = 5;
-	while(h--){
-    i = (stack*)malloc(sizeof(stack));
-	i->patates = h;
-	i->next = NULL;}
-	return i;
+// Push işlemi
+void push(t_stack **stack, int value)
+{
+    t_stack *new_elem;
+
+    new_elem = (t_stack*)malloc(sizeof(t_stack));
+    new_elem->value = value;
+    new_elem->next = *stack;
+    *stack = new_elem;
+}
+
+// Pop işlemi
+int pop(t_stack **stack)
+{
+    int value;
+    t_stack *temp;
+
+    if (!*stack)
+        return (-1);
+    value = (*stack)->value;
+    temp = *stack;
+    *stack = (*stack)->next;
+    free(temp);
+    return (value);
 }
 
 int main()
 {
-	stack *ag = create();
-    printf("%d", ag->next->patates);
-    return 0;
+    t_stack *stack;
+    int arr[] = {1, 3, 5, 2};
+    int i;
+
+    stack = NULL;
+    // Dizideki elemanları yığına ekleyin
+    for (i = 0; i < sizeof(arr)/sizeof(int); i++)
+        push(&stack, arr[i]);
+
+    // Yığındaki elemanları ekrana yazdırın
+    while (stack)
+        printf("%d ", pop(&stack));
+
+    return (0);
 }
