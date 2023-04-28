ab9854fc4105059132b660a58b5d3b32671121a5
Selim Yildirim <seyildir@f0r4s22.codam.nl>
2023-04-28 18:10:51 +0200

    codam

--- algorithm.c
+++ algorithm.c
@@ -3,43 +3,12 @@
 
 void	little_short(stack **a, stack **b)
 {
-	stack *temp;
-
-	temp = *a;
-	int i = 0;
-	int count = 0;
-	while (i < 5)
+	stack *mem = *a;
+	while ((*a)->next)
 	{
-		while (temp->next)
-		{
-			if(temp->num > temp->next->num)
-				sa(a);
-			temp = temp->next;
-		}
-		pb(a, b);
-		i++;
-		count++;
-	}
-	i = 0;
-	temp = *b;
-	while (i < count)
-	{
-		while (temp->next)
-		{
-			if(temp->num < temp->next->num)
-				rrb(b);
-			temp = temp->next;
-		}
-		pa(a, b);
-		i++;
-	}
-	
-	temp = *a;
-	while (temp->next)
-	{
-		if (temp->num > temp->next->num)
+		if((*a)->num > (*a)->next->num)
 			sa(a);
-		temp = temp->next;
+		*a = (*a)->next;
 	}
-	
+	*a = mem;
 }
